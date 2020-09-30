fd657376a3435c4ca117948c16baa15bbc55d587
unknown <mat_goulart3003@yahoo.com.br>
2020-09-30 17:00:00 -0300

    incompleto porem avancei alguns passos

--- trabalhoMatriz/src/trabalhoMatrizJavaGaabi/StartApp.java
+++ trabalhoMatriz/src/trabalhoMatrizJavaGaabi/StartApp.java
@@ -4,29 +4,30 @@ import java.util.Scanner;
 
 public class StartApp {
 	
+	static int pos = 0;
 	static int tamanhoLinha = 10;
 	static int tamanhoColuna = 20;
 	static Scanner scan = new Scanner (System.in);
 	
 	public static void main(String[] args) {
 		
-		char matriz [][] = new char [tamanhoLinha][tamanhoColuna];
+		String matriz [][] = new String [tamanhoLinha][tamanhoColuna];
 		
 		preencheMatriz1(matriz);
 		imprimeEsteira(matriz);
-		
+		passaEsteira(matriz);
 		
 		
 		scan.close();
 	}
 	
-	public static void preencheMatriz1 (char matriz [][]) {
+	public static void preencheMatriz1 (String matriz [][]) {
 		
 		for (int i = 0; i < tamanhoLinha; i++) {
 			
 			for (int j = 0; j < tamanhoColuna; j++) {
 				
-				matriz [i][j] = '-';
+				matriz [i][j] = "-";
 				
 			}
 			
@@ -35,7 +36,7 @@ public class StartApp {
 		
 	}
 	
-	public static void imprimeEsteira (char matriz [][]) {
+	public static void imprimeEsteira (String matriz [][]) {
 		
 		for (int i = 0; i < tamanhoLinha; i++) {
 			
@@ -51,10 +52,60 @@ public class StartApp {
 		
 	}
 	
-	public static void passaEsteira (char matriz [][]) {
+	public static void passaEsteira (String matriz [][]) {
 		
+		for (int i = 0; i < tamanhoLinha; i++) {
+			
+			for (int j = 0; j < tamanhoColuna; j++) {
+				
+				String enter = scan.nextLine();				
+				
+				if (enter.equals("")) {
+					
+					if (j < 7) {
+					
+						imprimeFase("A",matriz, i,j);
+
+					}else if (j < 11) {					
+					
+						imprimeFase("B",matriz, i,j);
+
+			
+					}else if (j < 16) {	
+											
+						imprimeFase("C",matriz, i,j);
+
+				
+					}else {
+					
+						imprimeFase("D",matriz, i,j);
+
+					
+					}
+					
+				
+					
+				}
+				
+			}
+					
+		}
 		
+	}		
+
+	public static void imprimeFase (String letra, String matriz [][], int i, int j) {
 		
+		System.out.println("Você está na fase: " + letra + ", e na esteira " + (j+1));
+		matriz [i][j] = letra;
+		imprimeEsteira(matriz);
+		matriz [i][j] = "-";
 		
 	}
+
+	
+
+
+
+
 }
+
